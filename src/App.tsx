bc2072929aee4e016728204db9ea15ce4fbad42c
Kiryl Kazak <kazak.kiryl@sqilsoft.by>
2022-08-28 17:59:56 +0300

    Added eslint in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
 import {Route, Routes} from 'react-router';
-import './App.css';
+import { Provider } from 'react-redux';
 import {routes} from './constants/routePath';
 import {Translator} from './pages/Translator';
 import {Favourite} from './pages/Favourite';
 import {BrowserRouter} from 'react-router-dom';
 import {NotFoundPage} from './pages/NotFoundPage';
 import {Header} from './components/Header';
+import {store} from './store';
+import {initApp} from './actions/initApp/initApp';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+store.dispatch(initApp)
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
         <Header />
-
         <Routes>
           <Route path={routes.root} element={<Translator/>}/>
           <Route path={routes.favourite} element={<Favourite/>}/>
           <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
